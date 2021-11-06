b6de351c7790afdb58ad620b0cf49a20ec01c731
leoguzi <leoguzi@hotmail.com>
2021-11-06 16:08:37 -0300

    Initial configuration

---
+++ src/setup.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+
+let envFile = '';
+
+if (process.env.NODE_ENV === 'dev') {
+  envFile = '.env.dev';
+}
+
+if (process.env.NODE_ENV === 'test') {
+  envFile = '.env.test';
+}
+
+dotenv.config({
+  path: envFile,
+});
